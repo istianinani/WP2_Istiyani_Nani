b3ef3e234a897b355794af93c8f06ed85ceef537
istianinani <istianinani29@gmail.com>
2021-09-29 22:06:38 +0700

    pertemuan 3

---
+++ application/views/v_about.php
@@ -0,0 +1,22 @@
+<section>
+ <h1><?php echo $judul ?></h1>
+ <h4>Nama</h4>
+ <ul type="disc">
+ <li>Nama Depan : Istiyani </li>
+ <li>Nama Belakang : Nani</li>
+ </ul>
+ <br>
+ <h4>Alamat</h4>
+ <ul type="none">
+ <li> Kp pulogadung, jakarta timur, Dki Jakarta</li>
+ </ul>
+
+ <h4>Tempat Lahir</h4>
+ <ul type="none">
+ <li>Jakarta</li>
+ </ul>
+ <h4>Olah Raga Favorit</h4>
+ <ul type="square">
+ <li>berenang</li>
+ </ul>
+</section>
