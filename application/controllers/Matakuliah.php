6e744a8c7ffb08ed983df607128c8c1a24592739
istianinani <istianinani29@gmail.com>
2021-10-11 18:26:25 +0700

    pertemuan 5

--- application/controllers/Matakuliah.php
+++ application/controllers/Matakuliah.php
@@ -5,8 +5,22 @@ class matakuliah extends CI_Controller
   {
     $this->load->view('view-form-matakuliah');
   }
-  public function cetak()
-  {
+   public function cetak()
+   {
+     $this->form_validation->set_rules('kode', 'Kode Matakuliah',
+     'required|min_length[3]', [
+     'required' => 'Kode Matakuliah Harus diisi',
+     'min_lenght' => 'Kode terlalu pendek'
+     ]);
+     $this->form_validation->set_rules('nama', 'Nama Matakuliah',
+    'required|min_length[3]', [
+     'required' => 'Nama Matakuliah Harus diisi',
+     'min_lenght' => 'Nama terlalu pendek'
+     ]);
+
+     if ($this->form_validation->run() != true) {
+     $this->load->view('view-form-matakuliah');
+     } else {
     $data = [
       'kode' => $this->input->post('kode'),
       'nama' => $this->input->post('nama'),
@@ -14,4 +28,5 @@ class matakuliah extends CI_Controller
     ];
     $this->load->view('view-data-matakuliah', $data);
   }
+}
 }
--- application/views/view-form-matakuliah.php
+++ application/views/view-form-matakuliah.php
@@ -5,6 +5,8 @@
  </head>
  <body>
     <center>
+      <?php echo validation_errors(); ?>
+
           <form action="<?= base_url('matakuliah/cetak'); ?>" method="post">
                <table>
                   <tr>
